4bb53893e38ab02f2371f69cd90b104bbc081577
AlessandroMuscatello <64227776+AlessandroMuscatello@users.noreply.github.com>
2022-08-19 22:54:47 +0200

    started writing python code

--- python/init.py
+++ python/init.py
@@ -1,5 +1,8 @@
 from flask import Flask
+
 import os
+import cv2
+from cv_code import extract_and_predict_faces
 
 hostName = os.environ.get('HOST_NAME') or "0.0.0.0"
 serverPort = os.environ.get('SERVER_PORT') or 8080
@@ -8,10 +11,17 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def main():
+def default():
     isdir = os.path.isdir(os.environ.get('IMAGE_STORAGE'))
     return "lo è: " + str(isdir)
 
+@app.route('/test')
+def testFunc():
+    image = cv2.imread("test5.jpg")
+    predictedImage = extract_and_predict_faces(image)
+    print("predictedImage")
+    cv2.imwrite("./test5Predicted.jpg", predictedImage)
+    return "FATTO"
 
 app.run(host=hostName, port=serverPort)
 
